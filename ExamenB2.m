d329fdc8f68964ac8e352db80cd36b13318695ce
OscarGordillo <55550346+OscarGordillo@users.noreply.github.com>
2019-10-25 01:08:47 -0500

    Add files via upload

---
+++ ExamenB2.m
@@ -0,0 +1,26 @@
+clear, clc
+syms x
+cf=('3(x+1)^-1');
+f=inline(cf);
+derivada=diff(cf,x);
+df=inline(derivada);
+tol=0.001;
+error=50;
+
+x=0;
+n=0;
+disp(' n xi error')
+fprintf('\t%i\t%3.5f\t%f\n',n,x,error);
+n=n+1;
+x=x-f(x)/df(x);
+error=abs(f(x));
+
+tam = size(error);
+ite = 1:1:tam(2);
+figure('DefaultAxesFontSize',14)
+plot(ite,error,'color',[1 0 0],'LineWidth',2);
+set(gcf,'color','red');
+grid on
+xlabel 'iteraciones'
+ylabel 'error'
+title 'ERROR'
