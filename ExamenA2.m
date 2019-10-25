250c058ed74f73a15c1f47499422281faffba509
OscarGordillo <55550346+OscarGordillo@users.noreply.github.com>
2019-10-25 01:16:05 -0500

    Add files via upload

--- ExamenA2.m
+++ ExamenA2.m
@@ -1,22 +1,29 @@
-x0=0
-f=3/(x+1);
-i=1;
-fx(i)=x0;
+clear, clc
+syms x
+cf=('sin(x)+cos(x)-1.2');
+f=inline(cf);
+derivada=diff(cf,x);
+df=inline(derivada);
+tol=0.001;
+error=50;
 
-syms x;
-f1=subs(f,x,fx(i));
-z=diff(f);
-d=subs(z,x,fx(i));
+x=0;
+n=0;
+disp(' n xi error')
+fprintf('\t%i\t%3.5f\t%f\n',n,x,error);
+n=n+1;
+x=x-f(x)/df(x);
+error=abs(f(x));
 
-ea(1)=100;
-
-while abs(ea(i))>=.001;
-    fx(i+1)=fx(i)-f1/d; f1=subs(f,x,fx(i+1)); d=subs(z,x,fx(i+1));
-    ea(i+1)=abs((fx(i+1)-fx(i))/fx(i+1)*100);
-    i=i+1;
-end
-
-
+tam = size(error);
+ite = 1:1:tam(2);
+figure('DefaultAxesFontSize',14)
+plot(ite,error,'color',[1 0 0],'LineWidth',2);
+set(gcf,'color','white');
+grid on
+xlabel 'iteraciones'
+ylabel 'error'
+title 'ERROR'
 
 
 
